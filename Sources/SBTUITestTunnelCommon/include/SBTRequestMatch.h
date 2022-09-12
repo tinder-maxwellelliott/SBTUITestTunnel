13d90c1c689cd78241523cbd72bda8274d2f3b03
Maxwell Elliott <maxwell.elliott@gotinder.com>
2022-09-12 11:40:05 -0400

    use non modular import

--- Sources/SBTUITestTunnelCommon/include/SBTRequestMatch.h
+++ Sources/SBTUITestTunnelCommon/include/SBTRequestMatch.h
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-@import Foundation;
+#import <Foundation/Foundation.h>
 
 @interface SBTRequestMatch: NSObject<NSCoding, NSCopying>
 
